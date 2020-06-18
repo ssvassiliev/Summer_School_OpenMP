936bcf607a114f2c68ad8e33bb5bb0792e8ed09e
Serguei Vassiliev <serguei.vassiliev@ace-net.ca>
2020-06-18 16:52:22 -0300

    add task_depend

--- code/task_depend_omp.c
+++ code/task_depend_omp.c
@@ -6,42 +6,41 @@
 
 int main(int argc, char **argv) {
 
-    int N = 10;
-    int x[N][N];
-    int i,j;
+  int N = 10;
+  int x[N][N];
+  int i,j;
 
-    for(i=0;i<N;i++)
-	for(j=0;j<N;j++)
-	    x[i][j]=i+j;
+  /* Initialize matrix */
+  for(i=0;i<N;i++)
+    for(j=0;j<N;j++)
+      x[i][j]=i+j;
 
-printf("serial\n");
-    for(i=1;i<N;i++){
-	for(j=1;j<N;j++){
-	    x[i][j] = x[i-1][j] + x[i][j-1];
-	    printf("%8d ",x[i][j]);
-	}
-	printf("\n");
+  printf("Serial:\n");
+  for(i=1;i<N;i++){
+    for(j=1;j<N;j++){
+      x[i][j] = x[i-1][j] + x[i][j-1];
+      printf("%8d ",x[i][j]);
     }
-
-
-    for(i=0;i<N;i++)
-	for(j=0;j<N;j++)
-	    x[i][j]=i+j;
     printf("\n");
+  }
+
+  /* Reinitialize matrix */
+  for(i=0;i<N;i++)
+    for(j=0;j<N;j++)
+      x[i][j]=i+j;
+  printf("\n");
 
 #pragma omp parallel
 #pragma omp single
-    for(i=1;i<N;i++)
-	for(j=1;j<N;j++)
+  for(i=1;i<N;i++)
+    for(j=1;j<N;j++)
 #pragma omp task depend(out:x)
-	    x[i][j] = x[i-1][j] + x[i][j-1];
+      x[i][j] = x[i-1][j] + x[i][j-1];
 
-printf("parallel\n");
-    for(i=1;i<N;i++){
-	for(j=1;j<N;j++)
-	    printf("%8d ",x[i][j]);
-	printf("\n");
-    }
-
-
+  printf("Parallel:\n");
+  for(i=1;i<N;i++){
+    for(j=1;j<N;j++)
+      printf("%8d ",x[i][j]);
+    printf("\n");
+  }
 }
